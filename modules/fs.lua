30ed9b7902eb90f7edbef44247850cbd5c8039d9
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-20 16:52:43 -0500

    Create fs.lua

---
+++ modules/fs.lua
@@ -0,0 +1,46 @@
+local collection = {}
+
+local function parseFileName(p)
+    local ret = {}
+    local slashy = p:split('/')
+    for i, v in pairs(slashy) do
+        if i ~= #slashy then
+            table.insert(ret, {v, "folder"})
+        else
+            if #v:split('.') > 1 then
+                table.insert(ret, {v, "file"})
+            else
+                table.insert(ret, {v, "folder"})
+            end
+        end
+    end
+    return ret
+end
+
+collection.pathExists = function(f, callback)
+    local pathParsed = parseFileName(f)
+    if pathParsed[#pathParsed][2] == "folder" then
+        local is = isfolder(f)
+        if callback then callback(nil, is) end; return is
+    else
+        local is = isfile(f)
+        if callback then callback(nil, is) end; return is
+    end
+end
+collection.makeFile = function(f, callback)
+    local pathParsed = parseFileName(f)
+    local pathCompleted = ""
+    for _, v in pairs(pathParsed) do
+        pathCompleted = pathCompleted.. "/".. v[1]
+        if v[2] == "folder" then makefolder(pathCompleted) 
+        else writefile(pathCompleted, "") end
+    end
+    if callback then callback(nil, true) end; return true
+end
+collection.writeFile = function(f, c, callback)
+    if not collection.pathExists(f) then collection.makeFile(f) end
+    writefile(f, c)
+    if callback then callback(nil, true) end; return true
+end
+
+return collection
