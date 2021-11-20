7e8fc94829a48d248b40548e59add53fa9586856
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-19 19:38:44 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local existingModules = {"client"}
 return {
     new = function(m)
         for _, v in pairs(existingModules) do
-			if v == m then return loadstring(game:HttpGet("https://raw.githubusercontent.com/Mlemix/roblos/main/modules/%s.lua"):format(m))
-		end
+		if v == m then return loadstring(game:HttpGet("https://raw.githubusercontent.com/Mlemix/roblos/main/modules/%s.lua"):format(m)) end
+	end
     end
 }
