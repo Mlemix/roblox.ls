15b3e6b4f3cd9bbd5935029273d181c082fb283a
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-19 20:11:51 -0500

    Update client.lua

--- modules/client.lua
+++ modules/client.lua
@@ -1,5 +1,19 @@
-return {
-	test = function()
-		return "Penis"
+local collection = {}
+
+local plrs = game:GetService("Players")
+local lp = plrs.LocalPlayer
+
+collection.on = function(t, callback)
+	if not t then return "A type is required." end
+	if t == "respawn" then
+		lp.CharacterAdded:Connect(function()
+			callback(lp.Character)
+		end)
+	elseif t == "leave" then
+		plrs.PlayerRemoving:Connect(function(plr)
+			callback(plr)
+		end)
 	end
-}
+end
+
+return collection
