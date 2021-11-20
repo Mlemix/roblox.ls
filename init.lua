76edc24f40d45e7353ca0c5a555a231e664e8951
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-20 17:00:21 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 local existingModules = {"client", "fs"}
 
 if _G.roblox_ls then print("already loaded!") return _G.roblox_ls end
-return {
+_G.roblox_ls = {
 	new = function(m)
 		for _, v in pairs(existingModules) do
 			if v == m then return loadstring(game:HttpGet("https://raw.githubusercontent.com/Mlemix/roblos/main/modules/".. m ..".lua")) end
 		end
-	return nil 
+		return nil 
 	end
 }
+return _G.roblox_ls
