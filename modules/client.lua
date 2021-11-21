8512d264853a9f90c2bce98186828194c6e52a89
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-21 16:41:31 -0500

    Update client.lua

--- modules/client.lua
+++ modules/client.lua
@@ -3,6 +3,19 @@ local collection = {}
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
 
+local callbacks = {
+    CharacterAdded = {},
+	PlayerRemoving = {}
+}
+
+local function listenToPlayerEvents(plr)
+    plr.CharacterAdded:Connect(function(c)
+        for _, v in pairs(callbacks.CharacterAdded) do
+            v(c)
+        end
+    end)
+end
+
 collection.use = function(thing, args)
 	local type = typeof(thing)
 	if type == "function" then
@@ -15,15 +28,24 @@ end
 
 collection.on = function(t, callback)
 	if not t then return "A type is required." end
+	if not callback or typeof(callback) ~= "function" then return end
 	if t == "respawn" then
-		lp.CharacterAdded:Connect(function()
-			callback(lp.Character)
-		end)
+		table.insert(callbacks.CharacterAdded, callback)
 	elseif t == "leave" then
-		plrs.PlayerRemoving:Connect(function(plr)
-			callback(plr)
-		end)
+		table.insert(callbacks.PlayerRemoving, callback)
 	end
 end
+
+for _, v in pairs(plrs:GetPlayers()) do
+	listenToPlayerEvents(v)
+end
+plrs.PlayerAdded:Connect(function(plr)
+	listenToPlayerEvents(plr)
+end)
+plrs.PlayerRemoving:Connect(function(plr)
+	for _, v in pairs(callbacks.PlayerRemoving) do
+		v(plr)
+	end
+end)
 
 return collection
