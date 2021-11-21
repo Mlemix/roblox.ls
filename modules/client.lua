abac0b4bbb932913f64af024b0ab82eb655034f4
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-21 10:34:57 -0500

    Update client.lua

--- modules/client.lua
+++ modules/client.lua
@@ -3,6 +3,13 @@ local collection = {}
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
 
+collection.use = function(thing)
+	local type = typeof(thing)
+	if type == "function" then
+		coroutine.wrap(thing)()
+	end
+end
+
 collection.on = function(t, callback)
 	if not t then return "A type is required." end
 	if t == "respawn" then
