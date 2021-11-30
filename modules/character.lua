2d8ec62f6d5d19e00711bf481099d94ac43968f6
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-11-29 21:52:55 -0500

    Update character.lua

--- modules/character.lua
+++ modules/character.lua
@@ -1,4 +1,5 @@
 local rs = game:GetService('RunService')
+local ts = game:GetService("TweenService")
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
 
@@ -6,18 +7,22 @@ local collection = {
     noClip = false,
 }
 
-collection.tweenTP = function(to, t, back, callback)
+collection.tweenTP = function(to, t, back, callback, callbackonback)
         if collection.noClip then sbnc = true end
         local hrp = lp.Character.HumanoidRootPart
         local op = CFrame.new(hrp.Position)
         
         collection.noClip = true
         
-        game:GetService("TweenService"):Create(hrp, TweenInfo.new(t, Enum.EasingStyle.Linear), {CFrame = to}):Play()
+        ts:Create(hrp, TweenInfo.new(t, Enum.EasingStyle.Linear), {CFrame = to}):Play()
         wait(t + 0.1)
         if callback then callback() end
-        if back then game:GetService("TweenService"):Create(hrp, TweenInfo.new(t, Enum.EasingStyle.Linear), {CFrame = op}):Play() end
-        if not sbnc then collection.noClip = false end
+	wait(0.2)
+	if not sbnc then collection.noClip = false end
+	if not back then return end
+        ts:Create(hrp, TweenInfo.new(t, Enum.EasingStyle.Linear), {CFrame = op}):Play()
+	wait(t + 0.1)
+	if callbackonback then callbackonback() end
     end
 
 rs.Stepped:Connect(function()
