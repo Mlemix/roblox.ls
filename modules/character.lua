3afae6052a148ba2aeb48fae81eb6dd21d643948
Mlemix <94724870+Mlemix@users.noreply.github.com>
2021-12-05 12:13:19 -0500

    Update character.lua

--- modules/character.lua
+++ modules/character.lua
@@ -2,6 +2,7 @@ local rs = game:GetService('RunService')
 local ts = game:GetService("TweenService")
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
+local cam = workspace:WaitForChild("CurrentCamera")
 
 local collection = {
     noClip = false,
@@ -23,7 +24,24 @@ collection.tweenTP = function(to, t, back, callback, callbackonback)
         ts:Create(hrp, TweenInfo.new(t, Enum.EasingStyle.Linear), {CFrame = op}):Play()
 	wait(t + 0.1)
 	if callbackonback then callbackonback() end
+end
+
+collection.findCharacterInFOV = function(fov, othercheck)
+    plr = nil
+    for _, v in pairs(plrs:GetPlayers()) do
+        if v ~= lp and v.Character and v.Character:FindFirstChild("Head") and (othercheck and othercheck(v) or true) then
+            local pos, visible = cam:WorldToScreenPoint(v.Character:FindFirstChild("Head").Position)
+            if visible then
+                local mouse = lp:GetMouse()
+                mag = (Vector2.new(pos.X, pos.Y) - Vector2.new(mouse.X, mouse.Y)).magnitude
+                if mag <= fov then
+                    plr = v
+                end
+            end
+        end
     end
+    return plr
+end
 
 rs.Stepped:Connect(function()
     if collection.noClip and lp.Character then
